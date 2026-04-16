d4c7008e054356b20e356056f6072dde4deb2e07
TheDutchman68 <nd527589@gmail.com>
2026-04-16 17:56:58 +0300

    feat: add dynamic login and logout behavior to sidebar using auth context

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,15 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
-import { Home, Briefcase, LogIn } from "lucide-react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Home, Briefcase, LogIn, ChevronLeft, ChevronRight, LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 function Sidebar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const location = useLocation();
-
+  const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuth();
 
   const toggleSidebar = () => {
     setIsCollapsed((prev) => !prev);
+  };
+
+  const handleLogout = () => {
+      logout();
+      navigate("/");
   };
 
   const isActive = (path) => location.pathname === path;
@@ -25,19 +32,35 @@ function Sidebar() {
 
       <nav>
         <Link className={isActive("/") ? "active" : ""} to="/" title={isCollapsed ? "Home" : ""}>
-          <Home size={20} className="sidebar-icon" />
+          <Home size={20} />
           <span className={`link-text ${isCollapsed ? "hidden" : ""}`}>Home</span>
         </Link>
 
         <Link className={isActive("/jobtracker") ? "active" : ""} to="/jobtracker" title={isCollapsed ? "Job Tracker" : ""}>
-          <Briefcase size={20} className="sidebar-icon" />
+          <Briefcase size={20} />
           <span className={`link-text ${isCollapsed ? "hidden" : ""}`}>Job Tracker</span>
         </Link>
 
-        <Link className={isActive("/login") ? "active" : ""} to="/login" title={isCollapsed ? "Login" : ""}>
-          <LogIn size={20} className="sidebar-icon" />
-          <span className={`link-text ${isCollapsed ? "hidden" : ""}`}>Login</span>
-        </Link>
+      {isAuthenticated ? (
+          <button
+            type="button"
+            className="sidebar-link"
+            onClick={handleLogout}
+            title={isCollapsed ? "Logout" : ""}
+          >
+            <LogOut size={20} />
+            <span className={`link-text ${isCollapsed ? "hidden" : ""}`}>Logout</span>
+          </button>
+            ) : (
+              <Link
+                className={isActive("/login") ? "active" : ""}
+                to="/login"
+                title={isCollapsed ? "Login" : ""}
+              >
+                <LogIn size={20} />
+                <span className={`link-text ${isCollapsed ? "hidden" : ""}`}>Login</span>
+              </Link>
+            )}
       </nav>
     </aside>
   );
